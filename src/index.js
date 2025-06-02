e95fe1ec0c8796b57302b782cc2bb846c61ddf08
Marialafu <lafuentelhmaria@gmail.com>
2025-06-02 12:00:49 +0200

    updates

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const port = 3000;
+
+const app = express();
+
+const usersFilePath = path.resolve(__dirname, '../data/users.json');
+
+app.listen(port, () => {
+  console.log('Server running on port ' + port);
+});
+
+app.get('/read', (req, res) => {
+  fs.readFile(usersFilePath, (error, data) => {
+    if (error) return res.status(500).send('Error al leer el archivo');
+
+    const jsonData = JSON.parse(data);
+    res.send(jsonData);
+  });
+});
+
+app.get('/write', (req, res) => {
+  const newUser = JSON.stringify({
+    userId: '1',
+    name: 'Susana Horia',
+    email: 'susanahoria@yahoo.com'
+  });
+
+  fs.readFile(usersFilePath, (error, data) => {
+    if (error) return res.status(500).send('Error al leer el archivo');
+    const jsonData = JSON.parse(data);
+
+    const jsonNewData = [...jsonData, newUser];
+    console.log(jsonNewData);
+    // fs.writeFile(usersFilePath, jsonNewData, error => {
+    //   if (error) return res.status(500).send('Error al leer el archivo');
+
+    //   res.end();
+    // });
+  });
+});
