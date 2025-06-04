0229425fb61663b6b5fcc0321d7290a9aabdaa7e
Marialafu <lafuentelhmaria@gmail.com>
2025-06-04 09:54:55 +0200

    update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { v4 } = require('uuid');
 const usersFilePath = path.resolve(__dirname, '../../data/users.json');
 
-const usersController = {}
+const usersController = {};
 
 usersController.readUsers = (req, res) => {
   fs.readFile(usersFilePath, (error, data) => {
@@ -12,77 +12,82 @@ usersController.readUsers = (req, res) => {
     const jsonData = JSON.parse(data);
     res.send(jsonData);
   });
-}
+};
 
 usersController.postNewUser = (req, res) => {
-    const {name, email} = req.body;
+  const { name, email } = req.body;
 
-    fs.readFile(usersFilePath, (error, data) => {
-        if (error) return res.status(500).send('Error al leer el archivo')
-        
-        const usersList = JSON.parse(data)
-        const newUser = {userId: v4(), name: name, email: email}
-        const newUsersList = JSON.stringify([...usersList, newUser])
+  fs.readFile(usersFilePath, (error, data) => {
+    if (error) return res.status(500).send('Error al leer el archivo');
 
-        const repeatedEmail = usersList.some(user => {
-            return user.email === newUser.email
-        })
+    const usersList = JSON.parse(data);
+    const newUser = { userId: v4(), name: name, email: email };
+    const newUsersList = JSON.stringify([...usersList, newUser]);
 
-        if (repeatedEmail) return res.status(409).send('Correo ya registrado')
+    const repeatedEmail = usersList.some(user => {
+      return user.email === newUser.email;
+    });
 
-        fs.writeFile(usersFilePath, newUsersList, error => {
-            if (error) return res.status(500).send('Error al escribir el archivo')
-        })
-    })
-}
+    if (repeatedEmail) return res.status(409).send('Correo ya registrado');
+
+    fs.writeFile(usersFilePath, newUsersList, error => {
+      if (error) return res.status(500).send('Error al escribir el archivo');
+    });
+  });
+};
 
 usersController.updateUser = (req, res) => {
-    const {email, name} = req.body
+  const { id } = req.params;
+  const { email } = req.body;
 
-    fs.readFile(usersFilePath, (error, data) => {
-        if (error) return res.status(500).send('Error al leer el archivo')
-        
-            const usersList = JSON.parse(data)
-            const updatedUser = usersList.find(user => {
-                const foundedUser = user.name === name && user
-                foundedUser.email = email
-                return foundedUser
-            })
-        
-            fs.writeFile(usersFilePath, JSON.stringify(updatedUser), error => {
-                if (error) return res.status(500).send('Error al actualizar el archivo')
-            })
+  fs.readFile(usersFilePath, (error, data) => {
+    if (error) return res.status(500).send('Error al leer el archivo');
 
-            console.log(updatedUser);
-            
-            
-            
-    })
+    if (email) {
+      const repeatedEmail = usersList.some(
+        user => user.userId !== id && user.email === email
+      );
+    }
+    if (repeatedEmail)
+      return res.status(500).send('Error al escribir el archivo');
 
-    res.send()
-}
+    const usersList = JSON.parse(data);
+    const updatedUser = usersList.map(user => {
+      if (user.userId === id) {
+        user = { ...req.body };
+        return user;
+      }
+      return user;
+    });
+
+    fs.writeFile(usersFilePath, JSON.stringify(updatedUser), error => {
+      if (error) return res.status(500).send('Error al actualizar el archivo');
+    });
+  });
+
+  res.send();
+};
 
 usersController.deleteUser = (req, res) => {
-    const {email, name} = req.body
+  const { id } = req.params;
 
-    fs.readFile(usersFilePath, (error, data) => {
-        if (error) return res.status(500).send('Error al leer el archivo')
-        
-            const usersList = JSON.parse(data)
-            const identifyUser = usersList.find(user => {
-                return user.name === name && user 
-            })
-            const newUsersList = usersList.filter(user => {
-                return user !== identifyUser
-            })
+  fs.readFile(usersFilePath, (error, data) => {
+    if (error) return res.status(500).send('Error al leer el archivo');
 
-        fs.writeFile(usersFilePath, JSON.stringify(newUsersList), error => {
-            if (error) return res.status(500).send('Error al escribir el archivo')
-        })
-    })
+    const usersList = JSON.parse(data);
+    const identifyUser = usersList.find(user => {
+      return user.userId === id && user;
+    });
+    const newUsersList = usersList.filter(user => {
+      return user !== identifyUser;
+    });
 
-    res.send()
+    fs.writeFile(usersFilePath, JSON.stringify(newUsersList), error => {
+      if (error) return res.status(500).send('Error al escribir el archivo');
+    });
+  });
 
-}
+  res.send();
+};
 
-module.exports = usersController
+module.exports = usersController;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const usersController = require('../controllers/user.controller');
 const usersRoutes = express.Router();
 
-usersRoutes.get('/read', usersController.readUsers)
-usersRoutes.post('/write', usersController.postNewUser)
-usersRoutes.patch('/patch', usersController.updateUser)
-usersRoutes.delete('/delete', usersController.deleteUser)
+usersRoutes.get('/read', usersController.readUsers);
+usersRoutes.post('/write', usersController.postNewUser);
+usersRoutes.patch('/update/:id', usersController.updateUser);
+usersRoutes.delete('/delete/:id', usersController.deleteUser);
 
-module.exports = usersRoutes
+module.exports = usersRoutes;
